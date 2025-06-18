fe525feffa3f2385c6069dc853a3fec6bb9b1ebc
sonugupta1234 <sonuguptahld037@gmail.com>
2025-06-18 16:37:22 +0530

    globalcontext

---
+++ Sprint-1/Day-1/GlobalfunctionContext.js
@@ -0,0 +1,16 @@
+
+var age = 25;
+
+function displayAge() {
+    console.log("Current Age:", age);
+}
+
+function changeAge() {
+    age = 30; 
+    console.log("Age Updated to:", age);
+}
+
+
+displayAge();  
+changeAge();    
+displayAge();   
